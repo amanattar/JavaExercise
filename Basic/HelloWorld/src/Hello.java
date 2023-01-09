cf1caabad043c068fd9bdac5ffa463e4e1341ad6
Aman <attar.aman29@gmail.com>
2023-01-08 19:45:35 -0600

    Hello world success

--- Basic/HelloWorld/src/Hello.java
+++ Basic/HelloWorld/src/Hello.java
@@ -1,2 +1,7 @@
 public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("Hello Aman");
+        System.out.println("Hello Jack");
+    }
 }
